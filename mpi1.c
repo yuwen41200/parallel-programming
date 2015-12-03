8f0b5ecdd3ab7293f695072cab94e7941339d80d
yuwen41200 <yuwen41200@gmail.com>
2015-12-03 20:13:36 +0800

    Learning MPI.

---
+++ mpi1.c
@@ -0,0 +1,17 @@
+/**
+ * Compile: mpicc -o mpi1 mpi1.c
+ * Execute: mpiexec -n 8 mpi1
+ */
+
+#include <stdio.h>
+#include "mpi.h"
+
+int main(int argc, char *argv[]) {
+	int rank, size;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	printf("Hello, World! from %d of %d\n", rank, size);
+	MPI_Finalize();
+	return 0;
+}
---
+++ mpi2.c
@@ -0,0 +1,31 @@
+/**
+ * Compile: mpicc -o mpi1 mpi2.c
+ * Execute: mpiexec -n 8 mpi2
+ */
+
+#include <stdio.h>
+#include "mpi.h"
+
+int main(int argc, char *argv[]) {
+	int rank, size;
+	char msg[100];
+	MPI_Status stat;
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	if (rank != 0) {
+		sprintf(msg, "process %d", rank);
+		MPI_Send(msg, strlen(msg) + 1, MPI_CHAR, /*dest*/ 0, /*tag*/ 0, MPI_COMM_WORLD);
+	}
+	else {
+		for (int i = 1; i < size; i++) {
+			MPI_Recv(msg, 100, MPI_CHAR, i, /*tag*/ 0, MPI_COMM_WORLD, &stat);
+			printf("%s\n", msg);
+		}
+		printf("process %d\n", rank);
+	}
+	MPI_Finalize();
+
+	return 0;
+}
