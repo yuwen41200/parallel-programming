553193a02039a87b7a68ee6a101a12abade6c9e2
yuwen41200 <yuwen41200@gmail.com>
2015-12-04 01:37:25 +0800

    Quality improved.

--- mpi5.c
+++ mpi5.c
@@ -14,5 +14,5 @@ int MPI_Type_vector(int count, int block_len, int stride, MPI_Datatype old_type,
 int MPI_Type_indexed(int count, int *arr_of_block_len, int *arr_of_displace, MPI_Datatype old_type,
                      MPI_Datatype *new_type);
 
-int MPI_Type_create_struct(int count, int *arr_of_block_len, MPI_Aint *array_of_displace,
+int MPI_Type_create_struct(int count, int *arr_of_block_len, MPI_Aint *arr_of_displace,
                            MPI_Datatype *array_of_old_type, MPI_Datatype *new_type);
