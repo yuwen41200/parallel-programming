939ad4d17b00596f08d844018b35be318fad4566
yuwen41200 <yuwen41200@gmail.com>
2015-12-17 23:32:16 +0800

    Optimize more.

--- cuda/cuda1.cu
+++ cuda/cuda1.cu
@@ -22,12 +22,12 @@ static void handleError(cudaError_t err, const char *file, int line) {
 
 void checkParam();
 __global__ void initLine(float*, float*, int);
-__global__ void updateAll(float*, float*, float*, int, int);
+__global__ void updateAll(float*, float*, int, int);
 void printResult();
 
 int totalSteps, totalPoints, allocPoints;
 float *currVal;
-float *devCurrVal, *devPrevVal, *devNextVal;
+float *devCurrVal, *devPrevVal;
 
 int main(int argc, char *argv[]) {
 	sscanf(argv[1], "%d", &totalPoints);
@@ -42,7 +42,6 @@ int main(int argc, char *argv[]) {
 
 	HANDLE_ERROR(cudaMalloc((void**) &devCurrVal, allocPoints * sizeof(float)));
 	HANDLE_ERROR(cudaMalloc((void**) &devPrevVal, allocPoints * sizeof(float)));
-	HANDLE_ERROR(cudaMalloc((void**) &devNextVal, allocPoints * sizeof(float)));
 
 	dim3 threadsPerBlock(256);
 	dim3 numOfBlocks(allocPoints/256);
@@ -51,7 +50,7 @@ int main(int argc, char *argv[]) {
 	initLine<<<numOfBlocks, threadsPerBlock>>>(devPrevVal, devCurrVal, totalPoints);
 
 	printf("Updating all points for all time steps...\n");
-	updateAll<<<numOfBlocks, threadsPerBlock>>>(devPrevVal, devCurrVal, devNextVal, totalPoints, totalSteps);
+	updateAll<<<numOfBlocks, threadsPerBlock>>>(devPrevVal, devCurrVal, totalPoints, totalSteps);
 
 	printf("Printing final results...\n");
 	HANDLE_ERROR(cudaMemcpy(currVal, devCurrVal, allocPoints * sizeof(float), cudaMemcpyDeviceToHost));
@@ -61,7 +60,6 @@ int main(int argc, char *argv[]) {
 
 	cudaFree(devCurrVal);
 	cudaFree(devPrevVal);
-	cudaFree(devNextVal);
 
 	free(currVal);
 
@@ -95,18 +93,19 @@ __global__ void initLine(float *__devPrevVal, float *__devCurrVal, int __totalPo
 	}
 }
 
-__global__ void updateAll(float *__devPrevVal, float *__devCurrVal, float *__devNextVal,
-                          int __totalPoints, int __totalSteps) {
+__global__ void updateAll(float *__devPrevVal, float *__devCurrVal, int __totalPoints, int __totalSteps) {
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	if (i < __totalPoints) {
+		float locPrevVal = __devPrevVal[i], locCurrVal = __devCurrVal[i] , locNextVal;
 		for (int j = 0; j < __totalSteps; j++) {
 			if ((i == 0) || (i == __totalPoints - 1))
-				__devNextVal[i] = 0.0;
+				locNextVal = 0.0;
 			else
-				__devNextVal[i] = 1.82 * __devCurrVal[i] - __devPrevVal[i];
-			__devPrevVal[i] = __devCurrVal[i];
-			__devCurrVal[i] = __devNextVal[i];
+				locNextVal = 1.82 * locCurrVal - locPrevVal;
+			locPrevVal = locCurrVal;
+			locCurrVal = locNextVal;
 		}
+		__devCurrVal[i] = locCurrVal;
 	}
 }
 
