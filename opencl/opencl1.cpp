bd93d09c08970e82d75cef7c126111ab8340a7ee
yuwen41200 <yuwen41200@gmail.com>
2015-12-31 03:14:27 +0800

    Try and error.

--- opencl/opencl1.cpp
+++ opencl/opencl1.cpp
@@ -41,46 +41,43 @@ int main() {
 //               OpenCL is the most ugly API I have ever seen.                //
 //----------------------------------------------------------------------------//
 
-	cl_platform_id   platform_id  = NULL ;
-	cl_uint          platform_num = 0    ;
-	cl_device_id     device_id    = NULL ;
-	cl_uint          devices_num  = 0    ;
-	cl_context       context      = NULL ;
-	cl_command_queue cmd_queue    = NULL ;
-	cl_mem           rst_mem      = NULL ;
-	cl_mem           img_mem      = NULL ;
-	cl_mem           sz_mem       = NULL ;
-	cl_program       program      = NULL ;
-	cl_kernel        kernel       = NULL ;
-	cl_int           value        = 0    ;
+	cl_platform_id   platform_id  = 0;
+	cl_uint          platform_num = 0;
+	cl_device_id     device_id    = 0;
+	cl_uint          devices_num  = 0;
+	cl_context       context      = 0;
+	cl_command_queue cmd_queue    = 0;
+	cl_mem           rst_mem      = 0;
+	cl_mem           img_mem      = 0;
+	cl_mem           sz_mem       = 0;
+	cl_program       program      = 0;
+	cl_kernel        kernel       = 0;
+	cl_int           ret          = 0;
 
-	value = clGetPlatformIDs(1, &platform_id, &platform_num);
-	value = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &devices_num);
-	context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &value);
-	cmd_queue = clCreateCommandQueue(context, device_id, 0, &value);
-	rst_mem = clCreateBuffer(context, CL_MEM_READ_WRITE, 768 * sizeof(unsigned int), NULL, &value);
-	img_mem = clCreateBuffer(context, CL_MEM_READ_WRITE, size * sizeof(unsigned int), NULL, &value);
-	sz_mem = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(unsigned int), NULL, &value);
-	value = clEnqueueWriteBuffer(cmd_queue, rst_mem, CL_TRUE, 0, 768 * sizeof(unsigned int), result, 0, NULL, NULL);
-	value = clEnqueueWriteBuffer(cmd_queue, img_mem, CL_TRUE, 0, size * sizeof(unsigned int), image, 0, NULL, NULL);
-	value = clEnqueueWriteBuffer(cmd_queue, sz_mem, CL_TRUE, 0, sizeof(unsigned int), size, 0, NULL, NULL);
-	program = clCreateProgramWithSource(context, 1, &code_char, &code_len, &value);
-	value = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
-	kernel = clCreateKernel(program, "histogram", &value);
-	value = clSetKernelArg(kernel, 0, sizeof(cl_mem), &rst_mem);
-	value = clSetKernelArg(kernel, 1, sizeof(cl_mem), &img_mem);
-	value = clSetKernelArg(kernel, 2, sizeof(cl_mem), &sz_mem);
-	value = clEnqueueTask(cmd_queue, kernel, 0, NULL, NULL);
-	value = clEnqueueReadBuffer(cmd_queue, rst_mem, CL_TRUE, 0, 768 * sizeof(unsigned int), result, 0, NULL, NULL);
-	value = clFlush(cmd_queue);
-	value = clFinish(cmd_queue);
-	value = clReleaseKernel(kernel);
-	value = clReleaseProgram(program);
-	value = clReleaseMemObject(rst_mem);
-	value = clReleaseMemObject(img_mem);
-	value = clReleaseMemObject(sz_mem);
-	value = clReleaseCommandQueue(cmd_queue);
-	value = clReleaseContext(context);
+	ret = clGetPlatformIDs(1, &platform_id, &platform_num);
+	ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &devices_num);
+	context = clCreateContext(0, 1, &device_id, 0, 0, &ret);
+	cmd_queue = clCreateCommandQueue(context, device_id, 0, &ret);
+	rst_mem = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, 768 * sizeof(unsigned int), result, &ret);
+	img_mem = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, size * sizeof(unsigned int), image, &ret);
+	sz_mem = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(unsigned int), &size, &ret);
+	program = clCreateProgramWithSource(context, 1, &code_char, &code_len, &ret);
+	ret = clBuildProgram(program, 1, &device_id, 0, 0, 0);
+	kernel = clCreateKernel(program, "histogram", &ret);
+	ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), &rst_mem);
+	ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), &img_mem);
+	ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), &sz_mem);
+	ret = clEnqueueTask(cmd_queue, kernel, 0, 0, 0);
+	ret = clEnqueueReadBuffer(cmd_queue, rst_mem, CL_TRUE, 0, 768 * sizeof(unsigned int), result, 0, 0, 0);
+	ret = clFlush(cmd_queue);
+	ret = clFinish(cmd_queue);
+	ret = clReleaseKernel(kernel);
+	ret = clReleaseProgram(program);
+	ret = clReleaseMemObject(rst_mem);
+	ret = clReleaseMemObject(img_mem);
+	ret = clReleaseMemObject(sz_mem);
+	ret = clReleaseCommandQueue(cmd_queue);
+	ret = clReleaseContext(context);
 
 //----------------------------------------------------------------------------//
 //                          End of these ugly codes.                          //
